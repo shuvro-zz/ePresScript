55cb39159e5f376b35f1a0ebe7fc36a9a2431791
akibur_fza <akibur_fza@yahoo.com>
2019-05-05 16:35:32 +0200

    tab impl

--- app/components/Settings.js
+++ app/components/Settings.js
@@ -1,29 +1,75 @@
-import React, { Component } from 'react';
-import Grid from '@material-ui/core/Grid';
+import React from 'react';
+import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+import PhoneIcon from '@material-ui/icons/Phone';
+import FavoriteIcon from '@material-ui/icons/Favorite';
+import PersonPinIcon from '@material-ui/icons/PersonPin';
+import HelpIcon from '@material-ui/icons/Help';
+import ShoppingBasket from '@material-ui/icons/ShoppingBasket';
+import ThumbDown from '@material-ui/icons/ThumbDown';
+import ThumbUp from '@material-ui/icons/ThumbUp';
+import Typography from '@material-ui/core/Typography';
+
 
 const styles = theme => ({
   root: {
     flexGrow: 1,
-    height: "100%",
+    height: "auto",
     marginTop: theme.spacing.unit * 8,
   },
 });
 
+function TabContainer(props) {
+  return (
+    <Typography component="div" style={{ padding: 8 * 3 }}>
+      {props.children}
+    </Typography>
+  );
+}
+
+TabContainer.propTypes = {
+  children: PropTypes.node.isRequired,
+};
 
 class Settings extends React.Component{
 
   constructor(props: Props, state: any) {
     super(props);
+    this.state = {
+      value: 0,
+    };
   }
-
+  handleChange = (event, value) => {
+    this.setState({ value });
+  };
   render(){
     const { classes } = this.props;
+    const { value } = this.state;
+
 
     return(
-      <Grid container className={classes.root}>
-        <h1> Settings View </h1>
-      </Grid>
+      <div className={classes.root}>
+        <AppBar position="static" color="default">
+          <Tabs
+            value={value}
+            onChange={this.handleChange}
+            variant="scrollable"
+            scrollButtons="on"
+            indicatorColor="primary"
+            textColor="primary"
+          >
+            <Tab label="Item One" icon={<PhoneIcon />} />
+            <Tab label="Item Two" icon={<FavoriteIcon />} />
+            <Tab label="Item Three" icon={<PersonPinIcon />} />
+          </Tabs>
+        </AppBar>
+        {value === 0 && <TabContainer>Item One</TabContainer>}
+        {value === 1 && <TabContainer>Item Two</TabContainer>}
+        {value === 2 && <TabContainer>Item Three</TabContainer>}
+      </div>
     )
   }
 }
